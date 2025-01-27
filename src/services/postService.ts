c320595240e97bc1b8f6719b0e6b5c7494a4e120
idan efraim <idan1000011@walla.com>
2025-01-28 00:59:15 +0200

    update

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,5 +1,5 @@
-import axios, { CanceledError } from "axios";
-export { CanceledError };
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export interface IPostResponse {
   title: string;
@@ -9,14 +9,16 @@ export interface IPostResponse {
 
 export const getPosts = async () => {
   if (localStorage.getItem("refreshToken") === null) {
-    localStorage.setItem(
-      "refreshToken",
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Nzk3ZDAxZjEyYTVhMWE0OTgyOWYyYmMiLCJyYW5kb20iOiIwLjMwNDE1MDIzNTU3OTI4NiIsImlhdCI6MTczODAwMjY5NiwiZXhwIjoxNzM4NjA3NDk2fQ.uTBfZCB541paxmv_BqzElZgFpCzG3K6v9VkD2Shbl-0"
-    );
-  }
+    // localStorage.setItem(
+    //   "refreshToken",
+    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Nzk3ZDAxZjEyYTVhMWE0OTgyOWYyYmMiLCJyYW5kb20iOiIwLjMwNDE1MDIzNTU3OTI4NiIsImlhdCI6MTczODAwMjY5NiwiZXhwIjoxNzM4NjA3NDk2fQ.uTBfZCB541paxmv_BqzElZgFpCzG3K6v9VkD2Shbl-0"
+    // );
 
-  const response = await axios.get<IPostResponse[]>("http://localhost:3000/");
-  return response.data;
+    //navigate("/login");
+  } else {
+    const response = await axios.get<IPostResponse[]>("http://localhost:3000/");
+    return response.data;
+  }
 };
 
 export default { getPosts };
